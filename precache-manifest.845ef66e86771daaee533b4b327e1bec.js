b1b7fcf86b80da75bd72fc2233706d1525656ba5
Pablo Emanuell <pabloemanuell2013@hotmail.com>
2019-04-28 22:19:10 -0300

    Updates

---
+++ precache-manifest.845ef66e86771daaee533b4b327e1bec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3406cd514107c6ecc3b4558cf2fc59",
+    "url": "/listaCompras/index.html"
+  },
+  {
+    "revision": "984b12bd7a6038ff354d",
+    "url": "/listaCompras/static/css/main.c80589d2.chunk.css"
+  },
+  {
+    "revision": "8a70600fc7fd61810736",
+    "url": "/listaCompras/static/js/2.5f344593.chunk.js"
+  },
+  {
+    "revision": "984b12bd7a6038ff354d",
+    "url": "/listaCompras/static/js/main.2fe1d268.chunk.js"
+  },
+  {
+    "revision": "e9251291ad9882b2726d",
+    "url": "/listaCompras/static/js/runtime~main.0e402d3a.js"
+  }
+]);
